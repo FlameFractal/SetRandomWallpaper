53d6756b1fbc082f6310b0a9175d848d382be791
FlameFractal <vishg8@gmail.com>
2017-01-21 20:28:36 +0530

    variable name consistent

--- setRandomBg.py
+++ setRandomBg.py
@@ -7,15 +7,15 @@ import urllib.request
 # Define some variables
 img_url = "https://unsplash.it/1920/1080/?random&myRandomParam="+str(datetime.datetime.now().time())
 img_name = "random.jpg" 
-image_path = "C:\\Users\\vishg\\Documents\\git\\SetRandomWallpaper\\"+img_name
+img_path = "C:\\Users\\vishg\\Documents\\git\\SetRandomWallpaper\\"+img_name
 
 
 
 # Get the image from the API !
-urllib.request.urlretrieve(img_url, img_name)
-print ("Image downloaded successfully at "+image_path)
+urllib.request.urlretrieve(img_url, img_path)
+print ("Image downloaded successfully at "+img_path)
 
 
 # Set the desktop wallpaper !
 SPI_SETDESKWALLPAPER = 20 
-print (ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3))
+print (ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, img_path, 3))
