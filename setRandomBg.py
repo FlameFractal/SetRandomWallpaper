63f4f7fc6ddabde8d2953d9ab6db88250052b2c3
FlameFractal <vishg8@gmail.com>
2017-01-27 10:39:03 +0530

    added gitignore for .jpg , save previously downloaded wallpapers

--- setRandomBg.py
+++ setRandomBg.py
@@ -1,21 +1,25 @@
 import os
 import ctypes
-import datetime
 import urllib.request
+import random
 
 
-# Define some variables
-img_url = "https://unsplash.it/1920/1080/?random&myRandomParam="+str(datetime.datetime.now().time())
-img_name = "random.jpg" 
+
+# Define some variables !
+print ("Initializing")
+img_url = "https://unsplash.it/1920/1080/?random"
+img_name = str(int((random.random()*10**5)))+".jpg" 
 img_path = "C:\\Users\\vishg\\Documents\\git\\SetRandomWallpaper\\"+img_name
 
 
 
 # Get the image from the API !
+print("Downloading image "+img_name)
 urllib.request.urlretrieve(img_url, img_path)
 print ("Image downloaded successfully at "+img_path)
 
 
+
 # Set the desktop wallpaper !
 SPI_SETDESKWALLPAPER = 20 
-print (ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, img_path, 3))
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, img_path, 3)
