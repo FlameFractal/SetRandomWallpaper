e617aa22602a67a0ff65db85dfafee428bb32c5d
FlameFractal <vishg8@gmail.com>
2017-10-04 16:42:54 +0530

    relative path, moved jpgs to subfolder

--- setRandomBg.py
+++ setRandomBg.py
@@ -4,20 +4,17 @@ import urllib.request
 import random
 
 
-
 # Define some variables !
 print ("Initializing")
 img_url = "https://unsplash.it/1920/1080/?random"
 img_name = str(int((random.random()*10**5)))+".jpg" 
-img_path = "C:\\Users\\vishg\\Documents\\git\\SetRandomWallpaper\\"+img_name
-
+img_path = os.path.dirname(os.path.abspath(__file__))+"\\pictures\\"+img_name
 
 
 # Get the image from the API !
 print("Downloading image "+img_name)
 urllib.request.urlretrieve(img_url, img_path)
 print ("Image downloaded successfully at "+img_path)
-
 
 
 # Set the desktop wallpaper !
